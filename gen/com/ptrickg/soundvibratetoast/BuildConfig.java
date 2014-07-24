e38bc4a209e87475e6544c680be6bb96e30f2aa4
pTricKg <ptrickg1976@gmail.com>
2014-07-24 16:00:12 -0600

    first commit

---
+++ gen/com/ptrickg/soundvibratetoast/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ptrickg.soundvibratetoast;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ptrickg/soundvibratetoast/MyBroadcastReceiver.java
@@ -0,0 +1,26 @@
+package com.ptrickg.soundvibratetoast;
+
+import com.ptrickg.soundvibratetoast.R;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Toast.makeText(context, "Irradiation of brain cells commence!.",
+        Toast.LENGTH_LONG).show();
+    // Vibrate the mobile phone
+    Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    vibrator.vibrate(2000);
+    
+    // Play something for tablet
+    MediaPlayer mPlayer = MediaPlayer.create(context, R.raw.braincells);
+    mPlayer.start();
+  }
+
+} 
